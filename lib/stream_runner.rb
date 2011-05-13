d5872aa59cbf772c31292b2ddeef83a89eb868c8
amikula <amikula@gmail.com>
2011-05-13 16:02:32 -0700

    Finding the correct version of the hadoop streaming jar in a more flexible fashion

--- lib/stream_runner.rb
+++ lib/stream_runner.rb
@@ -25,7 +25,7 @@ streaming_version = ENV['HADOOP_STREAMING_VERSION']
 streaming_version ="0.20.0" unless streaming_version
 
 HADOOP_HOME=ENV['HADOOP_HOME']
-HADOOP_STREAMING="#{HADOOP_HOME}/contrib/streaming/hadoop-#{streaming_version}-streaming.jar"
+HADOOP_STREAMING=Dir.glob("#{HADOOP_HOME}/contrib/streaming/hadoop*streaming.jar").find{|f| f.include?(streaming_version)}
 
 class StreamRunner
   def expand_path(file)
